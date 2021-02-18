1cb56e841848342a7c137a0cdc1262d47d4de9db
Eden Smith <edenwint@gmail.com>
2021-02-18 03:46:49 +0000

    new files

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,48 +10,54 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Eden',
+  subtitle: 'I am a Full Stack Developer who currently lives in Birmingham UK.',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'eden.png',
+  paragraphOne:
+    'I’m a focused and hardworking Junior Software Developer looking for an opportunity to develop further. A BootCamp graduate skilled in JavaScript, React, Node JS, Mongo DB, HTML & CSS, Command Line & Git; willing and eager to learn other development technologies.',
+  paragraphTwo:
+    'I have a strong technical professional with a Bachelor of Arts (BA) focused in Visual & Graphic Communication from Birmingham City University.',
+  paragraphThree:
+    'Eager not only for the invaluable practical skills and knowledge that my next role will offer but also for the additional development of my analytical thinking skills, problem solving abilities and the confidence to know that I can make a change, these are just as valuable and empowering.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'gowander.png',
+    title: 'GoWander',
+    info:
+      'The concept of GoWonder, is a simple way to explore a city. You simply type in your location, add a filter here and there, press the button, and it will generate an activity for you to do based on your filters and preferences.',
+    info2: 'The tools used were JavaScript, React, Node JS, MongDB and CSS ',
+    // url: '',
+    repo: 'https://https://github.com/Edenlsw/black-codher-personal-project', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'bookstore.png',
+    title: 'BookStore',
+    info:
+      'he premise of this project was to create a bookcase which allows users to search and filter through a list of books along with adding and deleting the books from their library. The main data was loaded using a JSON however a google books api was also added allowing the user to search for extra books not listed in the original JSON file.',
+    info2: 'The tools used were JavaScript, React, HTML, CSS and Bootstrap',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Edenlsw/BookStore/tree/master/mybookcase', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'quiz.png',
+    title: 'Hip-Hop Quiz',
+    info:
+      'The brief for this project was to create a quiz that loops through the array of questions to display the questions and options for the answer. Then to create a function that shows the results, score and changes colour depending on the being correct.',
+    info2: 'The tools used were JavaScript, HTML and CSS ',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Edenlsw/Quiz/tree/master/Quiz', // if no repo, the button will not show up
   },
 ];
 
@@ -59,36 +65,36 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'https://www.linkedin.com/in/eden-smith-wint/',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+    // {
+    //   id: nanoid(),
+    //   name: 'twitter',
+    //   url: '',
+    // },
+    // {
+    //   id: nanoid(),
+    //   name: 'codepen',
+    //   url: '',
+    // },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/eden-smith-wint/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://gist.github.com/Edenlsw',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
